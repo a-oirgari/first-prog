19c1125377ad02bec23e377abba8548b5c425b68
a-oirgari <abdrahmanghali1998@gmail.com>
2025-09-02 22:29:14 +0200

    the volume of the spher

---
+++ volume.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    
+    printf("le volume du sphère est:\t%f ",(4.0f/3.0f)*3.14*10*10*10);
+    
+    return 0;
+}
+//volume= 4/3*3.14*10*10*10 si en calcule par cette méthode on va tomber sur la conversion de type.
